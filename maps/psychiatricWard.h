e23643c2c84d7fe5d6778823cf7dfee6fa91947e
Luke Romp <rompl@uni.coventry.ac.uk>
2018-03-13 19:31:12 +0000

    Added the code for my floor

---
+++ maps/psychiatricWard.h
@@ -0,0 +1,66 @@
+//7 Rooms
+
+using namespace std;
+
+int currentArea=0;
+
+void PsychiatricRoom1()
+{
+    mvprintw(2, 70, "Doctor's Office");
+    currentArea=1;
+}
+
+void PsychiatricRoom2()
+{
+    mvprintw(2,70, "Double Doors");
+    currentArea=2;
+}
+
+void PsychiatricRoom3()
+{
+    mvprintw(2,70, "First Cell");
+    currentArea=3;
+}
+
+void PsychiatricRoom4()
+{
+    mvprintw(2,70, "Second Cell");
+    currentArea=4;
+}
+
+void PsychiatricRoom5()
+{
+    mvprintw(2,70, "Third Cell");
+    currentArea=5;
+}
+
+void PsychiatricRoom6()
+{
+    mvprintw(2,70, "Fourth Cell");
+        currentArea=6;
+}
+
+void PsychiatricRoom7()
+{
+    mvprintw(2,70, "Fifth Cell");
+    currentArea=7;
+}
+
+void PsychiatricRoom8()
+{
+    mvprintw(2,70, "Boiler Room");
+    currentArea=8;
+}
+
+//All the trigger points for the Story are above
+
+void checkArea(int x, int y)
+{
+    if(coorZ==1)
+    {
+        if ((x>=52)  &&(y>=3 ))
+        
+    }
+}
+    
+   
